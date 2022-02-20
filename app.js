4fcb903a57c2c1f17437ea7a3ef413129b88078a
afazio1 <alexa.fazio04@gmail.com>
2022-02-20 18:22:54 -0500

    custom error handler

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
 
 const mongoose = require("mongoose");
 
@@ -44,52 +45,65 @@ app.get("/about", (req, res) => {
 });
 
 // EDUCATION
-app.post("/education", async (req, res) => {
-    const edu = new Education(req.body);
+app.post("/education", catchAsync(async (req, res, next) => {
+
+    const edu = new Education({rats: "yes"});
     await edu.save();
     res.redirect("/education");
-});
+}));
 
-app.get("/education", async (req, res) => {
+app.get("/education", catchAsync(async (req, res) => {
     const educations = await Education.find({});
     res.render("educations/index", { educations });
-});
+}));
 
 app.get("/education/new", (req, res) => {
     res.render("educations/new");
 });
-app.put("/education/:id", async (req, res) => {
+app.put("/education/:id", catchAsync(async (req, res) => {
     await Education.findByIdAndUpdate({_id: req.params.id}, req.body);
     res.redirect("/education");
 
-})
-app.delete("/education/:id", async (req, res) => {
+}));
+
+app.delete("/education/:id", catchAsync(async (req, res) => {
     await Education.findByIdAndDelete({_id: req.params.id});
     res.redirect("/education");
-});
-app.get("/education/:id/edit", async (req, res) => {
+}));
+
+app.get("/education/:id/edit", catchAsync(async (req, res) => {
     const edu = await Education.findById({_id: req.params.id});
     res.render("educations/edit", { edu });
-});
+}));
 
 // EXPERIENCE
-app.post("/experience", async (req, res) => {
-    const exp = new Experience(req.body);
+app.post("/experience", catchAsync(async (req, res) => {
+    let {title, employer, link, stack, startDate, endDate} = req.body;
+    stack = stack.split(",");
+    const newExp = {
+        title: title,
+        employer: employer,
+        link: link,
+        stack: stack,
+        startDate: startDate,
+        endDate: endDate
+    }
+    const exp = new Experience(newExp);
     await exp.save();
     res.redirect("/experience");
-});
+}));
 
-app.get("/experience", async (req, res) => {
+app.get("/experience", catchAsync(async (req, res) => {
     const experiences = await Experience.find({});
     console.log(experiences);
     res.render("experiences/index", { experiences });
-});
+}));
 
 app.get("/experience/new", (req, res) => {
     res.render("experiences/new");
 });
-app.put("/experience/:id", async (req, res) => {
-    const {title, employer, link, stack, startDate, endDate} = req.body;
+app.put("/experience/:id", catchAsync(async (req, res) => {
+    let {title, employer, link, stack, startDate, endDate} = req.body;
     stack = stack.split(",");
     const newExp = {
         title: title,
@@ -103,15 +117,16 @@ app.put("/experience/:id", async (req, res) => {
     await Experience.findByIdAndUpdate({_id: req.params.id}, newExp);
     res.redirect("/experience");
 
-})
-app.delete("/experience/:id", async (req, res) => {
+}));
+app.delete("/experience/:id", catchAsync(async (req, res) => {
     await Experience.findByIdAndDelete({_id: req.params.id});
     res.redirect("/experience");
-});
-app.get("/experience/:id/edit", async (req, res) => {
+}));
+
+app.get("/experience/:id/edit", catchAsync(async (req, res) => {
     const exp = await Experience.findById({_id: req.params.id});
     res.render("experiences/edit", { exp });
-});
+}));
 
 // PROJECTS
 app.get("/projects", (req, res) => {
@@ -122,6 +137,9 @@ app.get("/projects/:id", (req, res) => {
     res.send("singular project");
 });
 
+app.use((err, req, res, next) => {
+    res.send("something went wrong")
+})
 app.listen(3000, () => {
     console.log("Serving on port 3000");
 });
