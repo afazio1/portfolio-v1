06185ce534c1736acf81941b03fa5c26d1211d78
afazio1 <alexa.fazio04@gmail.com>
2022-04-20 12:43:15 -0400

    keep alive moved to app.js

--- app.js
+++ app.js
@@ -166,3 +166,9 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Serving on port ${port}`);
 });
+
+// keep awake
+const http = require("http");
+setInterval(function () {
+    http.get("https://fast-ocean-50650.herokuapp.com/about");
+}, 1500000); // every 25 minutes
